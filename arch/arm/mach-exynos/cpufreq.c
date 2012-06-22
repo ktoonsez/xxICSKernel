4d986b118ef9faa971b81c8c78844b5265ba93ad
ktoonsez <ktoonsez@gmail.com>
2012-06-21 21:35:53 -0700

    little cpufreq change

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -264,7 +264,7 @@ int exynos_cpufreq_lock(unsigned int nId,
 	if (!exynos_info)
 		return -EPERM;
 
-	if (exynos_cpufreq_disable) {
+	if (exynos_cpufreq_disable && (nId != DVFS_LOCK_ID_TMU)) {
 		pr_info("CPUFreq is already fixed\n");
 		return -EPERM;
 	}
